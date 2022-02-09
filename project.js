d481e4e5251d5b7c5964c63a54ec8e6fb9955960
Eyob Mekonnen Nigussie <61557741+eyob-21@users.noreply.github.com>
2022-02-09 10:23:55 +0300

    Add files via upload

---
+++ project.js
@@ -0,0 +1,52 @@
+let projects = [
+ {
+     name: "project one",
+     tags: "#javascript, #fullstack, #ui/ux, #backend",
+     image: "project1.png",
+ },
+  {
+     name: "project two",
+     tags: "#javascript, #fullstack",
+     image: "project2.png",
+  },
+ {
+     name: "project three",
+     tags: " #ui/ux, #fullstack",
+     image: "project3.png",
+ },
+ {
+     name: "project four",
+     tags: " #ui/ux, #backend",
+     image: "project4.png",
+ },
+ {
+     name: "project five",
+     tags: " #fullstack, #ui/ux",
+     image: "project5.png",
+ },
+ {
+     name: "project six",
+     tags: "#javascript, ",
+     image: "project6.png"
+ },
+ {
+     name: "project seven",
+     tags: "#backend",
+     image: "project7.png",
+ },
+ {
+     name: "project eight",
+     tags: "#javascript, #fullstack,",
+     image: "project8.png",
+ },
+ {
+     name: "project nine",
+     tags: "#javascript, #fullstack, #backend",
+     image: "project9.png",
+ },
+ {
+     name: "project ten",
+     tags: "#javascript, #ui/ux, #backend",
+     image: "project10.png",
+ },
+];
